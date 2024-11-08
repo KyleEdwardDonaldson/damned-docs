e5fb4c1f4dddeb607e00c3c1862d9e856e8c05ff
Kyle Edward Donaldson <KyleEdwardDonaldson@gmail.com>
2024-11-08 07:31:56 -0600

    Deploy website - based on a1b0db2033e73c4224e8c3d5d83e71d26c90b97c

---
+++ assets/js/5dc08341.504dacab.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdamned_docs=self.webpackChunkdamned_docs||[]).push([[1475],{5203:(e,s,d)=>{d.r(s),d.d(s,{default:()=>c});d(6540);var a=d(4848);function c(){return(0,a.jsxs)("div",{style:{padding:"2rem"},children:[(0,a.jsx)("h1",{children:"Welcome to My New Page"}),(0,a.jsx)("p",{children:"This is a custom page added to the Docusaurus site."})]})}}}]);
